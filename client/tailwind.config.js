c58445982c731f9e346d239757c9a30f3adcdf31
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2026-02-27 19:54:26 +0530

    Added hero section with countdown

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        saffron: "#FF7A00",
+        gold: "#FFD700",
+        templeDark: "#0f0f0f"
+      }
+    },
   },
   plugins: [],
-};
+}
